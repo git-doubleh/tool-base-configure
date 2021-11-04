e8ad261d960f3f3f4123ce76253b94ab9dd966bb
黄浩 <huanghao@deltaphone.com.cn>
2021-11-04 14:55:07 +0800

    'init'

---
+++ index.js
@@ -0,0 +1,26 @@
+/*
+ * @Description:
+ * @Date: 2021-11-04 14:54:13
+ * @LastEditTime: 2021-11-04 14:54:13
+ */
+// 防抖
+function debounce(func, time) {
+  let timer = null;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, arguments);
+    }, time);
+  };
+}
+// 节流
+function throtte(func, time) {
+  let activeTime = 0;
+  return () => {
+    const current = Date.now();
+    if (current - activeTime > time) {
+      func.apply(this, arguments);
+      activeTime = Date.now();
+    }
+  };
+}
