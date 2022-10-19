557d2963bcef8d2a1bfe06c8f8d1ee6c7dc6e62e
hhuang <1874165806@qq.com>
2022-10-19 11:51:09 +0800

    添加prettier配置

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+/*
+ * @Description:  prettierrc
+ * @Date: 2022-10-10 09:44:00
+ * @LastEditTime: 2022-10-19 11:50:08
+ */
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: false,
+  printWidth: 80,
+  trailingComma: "none", 
+  bracketSpacing: true,
+  quoteProps: 'consistent',
+  htmlWhitespaceSensitivity: 'strict',
+  vueIndentScriptAndStyle: true,
+  endOfLine: 'auto'
+};
